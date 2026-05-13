2972fdc9ddac232bae5aa04e8c984265f34e308f
naresh <Naresh@gmail.com>
2026-05-13 22:11:22 +0530

    vpc module modification:tags

--- 01-vpc/envs/dev-mumbai/provider.tf
+++ 01-vpc/envs/dev-mumbai/provider.tf
@@ -10,7 +10,7 @@ terraform {
 # ✅ Default provider (MANDATORY)
 provider "aws" {
   region = var.region
-  profile = "role-jump-vpc-ec2-s3-ff"
+  #profile = "role-jump-vpc-ec2-s3-ff"
 
 /*    assume_role {
     role_arn = "arn:aws:iam::722979336094:role/terraform_aws_admin_role"
